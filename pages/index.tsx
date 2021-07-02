f6603685dfb7d8bc2f2ffb366e72a2958a391370
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-07-02 19:58:56 -0400

    Display logo and message on landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 import styled from 'styled-components';
 import Layout from '../components/Layout';
+import Spacer from '../components/Spacer';
+import gearLogo from '../public/images/gear-logo-teal.png';
+import { COLORS } from '../styles/constants';
 
 const Home = () => {
   return (
@@ -14,7 +19,28 @@ const Home = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <Layout>
-        <Main>Main content</Main>
+        <Main>
+          <ImageWrapper>
+            <Image
+              src={gearLogo}
+              alt='tulo.js gear logo'
+              width={400}
+              height={400}
+            />
+            <ImageShadow />
+          </ImageWrapper>
+          <Spacer size={100} />
+          <div>
+            <LandingTulo>tulo.js</LandingTulo>
+            <LandingHeading>
+              Making service workers easy so that your app is{' '}
+              <strong>fast and reliable, even offline.</strong>
+            </LandingHeading>
+            <Link href='/docs' passHref>
+              <DocsAnchor>Read the docs &rarr;</DocsAnchor>
+            </Link>
+          </div>
+        </Main>
       </Layout>
     </>
   );
@@ -25,7 +51,45 @@ const Main = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 2rem;
+`;
+
+const ImageWrapper = styled.div`
+  flex-shrink: 0;
+`;
+
+const ImageShadow = styled.div`
+  width: 100%;
+  border: solid ${COLORS.offWhite};
+  box-shadow: 0 48px 36px ${COLORS.darkGrey};
+`;
+
+const LandingTulo = styled.h1`
+  font-size: 5rem;
+  margin-bottom: 24px;
+`;
+
+const LandingHeading = styled.h2`
+  margin-bottom: 36px;
+  font-size: 2.5rem;
+  font-weight: 200;
+  font-style: italic;
+
+  & > strong {
+    color: ${COLORS.tealPrimary};
+  }
+`;
+
+const DocsAnchor = styled.a`
+  text-decoration: none;
+  color: ${COLORS.purplePrimary};
+  font-weight: 600;
+  font-size: 1.25rem;
+
+  &:hover {
+    color: ${COLORS.orangePrimary};
+    transform: scale(1.1);
+    transition: transform 0.3s ease-in-out;
+  }
 `;
 
 export default Home;
