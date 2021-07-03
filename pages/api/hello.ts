5e286a82976b122a9f62ad1709d9806f7620bd3f
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-07-03 15:34:38 -0400

    Test API route to receive a cross origin request and send a successful response

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,39 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// Next.js API route docs: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+// Next.js API middleware docs: https://nextjs.org/docs/api-routes/api-middlewares
+import Cors from 'cors';
 
-type Data = {
-  name: string
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  middlewareFunc: Function
+) {
+  return new Promise((resolve, reject) => {
+    middlewareFunc(req, res, (result: void | Error) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log({ headers: req.headers });
+
+  // run cors middleware
+  await runMiddleware(req, res, cors);
+
+  res.status(200).json({ hello: 'hello sent from /api/hello' });
 }
