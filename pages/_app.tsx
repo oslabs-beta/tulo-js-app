70398daea2ea7301f8ea5822d1089e41ffea8dc7
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-07-15 05:03:16 -0400

    Create web app manifest and sw for PWA; register sw in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import Head from 'next/head';
 import { GlobalStyle, CSSReset } from '../styles/globals';
 import type { AppProps } from 'next/app';
 import { Provider as AuthProvider } from 'next-auth/client';
@@ -5,16 +7,40 @@ import { Provider as ReduxProvider } from 'react-redux';
 import store from '../redux/store';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  // register service worker when App mounts
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((registration) =>
+          console.log(
+            `Service worker registered in scope: ${registration.scope}`
+          )
+        )
+        .catch((error) => {
+          console.log(`Error registering service worker: ${error}`);
+        });
+    }
+  }, []);
+
   return (
-    // Provider from next-auth allows user session state to be shared between pages
-    // Docs on next-auth client: https://next-auth.js.org/getting-started/client
-    <AuthProvider session={pageProps.session}>
-      <ReduxProvider store={store}>
-        <GlobalStyle />
-        <CSSReset />
-        <Component {...pageProps} />
-      </ReduxProvider>
-    </AuthProvider>
+    <>
+      <Head>
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='icon' type='image/png' href='/favicon.ico' />
+        <meta
+          name='description'
+          content='Making service workers easy so that your app is fast and reliable, even offline.'
+        />
+      </Head>
+      <AuthProvider session={pageProps.session}>
+        <ReduxProvider store={store}>
+          <GlobalStyle />
+          <CSSReset />
+          <Component {...pageProps} />
+        </ReduxProvider>
+      </AuthProvider>
+    </>
   );
 };
 
